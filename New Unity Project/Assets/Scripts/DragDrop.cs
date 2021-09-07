1918f4cea64b9f407775a6e89b228b0fc58014df
Cameron Burridge <cmburridge4022@gmail.com>
2021-09-07 15:19:43 -0600

    Fixed Click and Drag Started Return

--- New Unity Project/Assets/Scripts/DragDrop.cs
+++ New Unity Project/Assets/Scripts/DragDrop.cs
@@ -2,23 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class DragDrop : MonoBehaviour
 {
-    private Vector3 screenPoint;
-    private Vector3 offset;
-   void OnMouseDown()
-    {
-        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-        offset =  transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,screenPoint.z));
-    }
+    private Vector3 offsetPosition;
+    private Vector3 newPosition;
+    private Camera cam;
+    public Vector3Data data;
 
-    void OnMouseDrag()
-    {
-        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
-        transform.position = curPosition;
-    }
+    public bool CanDrag { get; set; }
+    public UnityEvent OnDrag;
+    public UnityEvent OnUp;
+    public bool Draggable { get; set; }
 
+    private void Start()
+    {
+        cam = Camera.main;
+        Draggable = true;
+    }
+    
+    public IEnumerator OnMouseDown()
+    {
+        OnDrag.Invoke();
+        offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
+        yield return new WaitForFixedUpdate();
+        CanDrag = true;
+        while (CanDrag)
+        {
+            yield return new WaitForFixedUpdate();
+            newPosition = cam.ScreenToWorldPoint(Input.mousePosition) + offsetPosition;
+            transform.position = newPosition;
+        }
+    }
+    
+    private void OnMouseUp()
+    {
+        transform.position = data.value;
+        CanDrag = false;
+        if (Draggable)
+        {
+            OnUp.Invoke();
+        }
+    }
 }
+
