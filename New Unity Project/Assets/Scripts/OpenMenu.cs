22cd51e4126bede35f8c803e10056f25bc3536f9
Cameron Burridge <cmburridge4022@gmail.com>
2021-09-14 11:53:56 -0600

    Added Mythos stat menus and icons with buttons

---
+++ New Unity Project/Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    public GameObject menu;
+    
+    public void menuSelect()
+    {
+        menu.SetActive(true);
+    }
+
+    public void closeMenu()
+    {
+        menu.SetActive(false);
+    }
+}
