7377ab0478d52a98147e7315c216fb012bbadffa
Cameron Burridge <cmburridge4022@gmail.com>
2021-09-28 13:29:14 -0600

    Attack Function

--- New Unity Project/Assets/Scripts/AttackBehavior.cs
+++ New Unity Project/Assets/Scripts/AttackBehavior.cs
@@ -6,25 +6,18 @@ public class AttackBehavior : MonoBehaviour
 {
     public Collectable target;
     public Collectable team;
-    int[] randomArray = new int[20];
-
+    public int randomValue;
+    
     public void Roll()
     {
-        
-        Debug.Log(target.power);
-
-        //for(int arrayIndex = 0; arrayIndex < randomArray.Length; arrayIndex++)
-        //{
-            //randomArray[arrayIndex] = Random.Range(0, 20);
-       // }
-        
-        //if (target.power >= randomArray[0])
-        //{
-            //Debug.Log("hit");
-       // }
-        //else
-       // {
-            //Debug.Log("miss");
-        //}
+        randomValue = Random.Range(1, 20);
+        if (team.power >= randomValue)
+        {
+            Debug.Log("hit");
+        }
+        else
+        {
+            Debug.Log("miss");
+        }
     }
 }
--- New Unity Project/Assets/Scripts/DragDrop.cs
+++ New Unity Project/Assets/Scripts/DragDrop.cs
@@ -17,17 +17,13 @@ public class DragDrop : MonoBehaviour
     public Vector3Data movePositionData;
     public Vector3Data scaleData;
     public Vector3Data newSpace;
-    public Vector3Data undoPosition;
-    
+
     public GameObject moveCheck;
     public GameObject movePosition;
-    //public GameObject undoButton;
-    //public GameObject buttons;
-
+    
     public Collectable thisMythos;
-
-    public ObjectData obj;
-
+    public Collectable teamate;
+    
     public bool CanDrag { get; set; }
     public UnityEvent OnDrag;
     public UnityEvent OnUp; 
@@ -37,15 +33,15 @@ public class DragDrop : MonoBehaviour
     {
         cam = Camera.main;
         Draggable = true;
-        obj.data = null;
     }
     
     public IEnumerator OnMouseDown()
     {
         movePositionData.value = moveCheck.transform.position;
         scaleData.value = transform.localScale;
-        obj.data = this.gameObject;
-
+        teamate.power = thisMythos.power;
+        teamate.defense = thisMythos.defense;
+        
         OnDrag.Invoke();
         offsetPosition = transform.position - cam.ScreenToWorldPoint(Input.mousePosition);
         yield return new WaitForFixedUpdate();
@@ -59,7 +55,6 @@ public class DragDrop : MonoBehaviour
             transform.localScale = newScale;
             moveCheck.SetActive(true);
             movePosition.SetActive(true);
-            //undoButton.SetActive(true);
             moveCheck.transform.position = movePositionData.value;
         }
     }
@@ -72,7 +67,6 @@ public class DragDrop : MonoBehaviour
         moveCheck.SetActive(false);
         movePosition.SetActive(false);
         moveCheck.transform.position = movePositionData.value;
-        undoPosition.value = positionData.value;
         transform.localScale = scaleData.value;
         if (Draggable)
         {
@@ -83,12 +77,6 @@ public class DragDrop : MonoBehaviour
     public void Move()
     {
         transform.position = newSpace.value;
-    }
-
-    public void Undo()
-    {
-        transform.position = undoPosition.value;
-        //undoButton.SetActive(false);
     }
 }
 
