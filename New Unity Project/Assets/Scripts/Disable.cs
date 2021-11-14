2e7062580ba0b9d0a0290f3ea2e70ae9351fd290
Cameron Burridge <cmburridge4022@gmail.com>
2021-11-13 23:16:15 -0700

    Edits

--- New Unity Project/Assets/Scripts/Disable.cs
+++ New Unity Project/Assets/Scripts/Disable.cs
@@ -12,7 +12,7 @@ public class Disable : MonoBehaviour
 
     private IEnumerator DisableThis()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(2);
         this.gameObject.SetActive(false);
     }
 }
